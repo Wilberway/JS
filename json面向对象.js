8534bfe389cffbccf316fea28c329661fb1cd01e
Wilberway <644645377@qq.com>
2014-11-04 16:41:16 +0800

    simple codes
    nothing~

---
+++ json面向对象.js
@@ -0,0 +1,23 @@
+<script>
+var miaov={};
+
+miaov.common={
+	getByClass:function()
+	{
+	},
+	myAddEvent:function()
+	{
+	}
+};
+
+miaov.fx={
+	startMove:function()
+	{
+	},
+	drag:function()
+	{
+	}
+};
+
+miaov.common.getByClass();
+</script>
